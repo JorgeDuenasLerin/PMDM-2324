b0b54e4a5918451612c94513b11badafc2146741
Jorge Dueñas Lerín <jorge.duenas.lerin@gmail.com>
2023-09-26 17:53:36 +0200

    Mi primera App

---
+++ app/src/main/java/com/example/pmdm_2324/ut01/ActividadSaluda.java
@@ -0,0 +1,16 @@
+package com.example.pmdm_2324.ut01;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.pmdm_2324.R;
+
+public class ActividadSaluda extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.ut01_actividad_saluda);
+    }
+}
