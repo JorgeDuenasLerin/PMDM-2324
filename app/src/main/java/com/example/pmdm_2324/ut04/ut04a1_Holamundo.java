e62f5773876ba3e7467272434981bed5ce58ec17
Jorge Dueñas Lerín <jorge.duenas.lerin@gmail.com>
2023-11-15 17:34:21 +0100

    UT04 Fragments

---
+++ app/src/main/java/com/example/pmdm_2324/ut04/ut04a1_Holamundo.java
@@ -0,0 +1,16 @@
+package com.example.pmdm_2324.ut04;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.pmdm_2324.R;
+
+public class ut04a1_Holamundo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.ut04a1_holamundo);
+    }
+}
