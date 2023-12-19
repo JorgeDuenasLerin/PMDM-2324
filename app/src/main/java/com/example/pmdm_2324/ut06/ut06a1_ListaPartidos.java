02da8ebe6a6ba179952f9a79836ec85e211319ae
Jorge Dueñas Lerín <jorge.duenas.lerin@gmail.com>
2023-12-19 17:50:26 +0100

    Listado de partido

---
+++ app/src/main/java/com/example/pmdm_2324/ut06/ut06a1_ListaPartidos.java
@@ -0,0 +1,42 @@
+package com.example.pmdm_2324.ut06;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.pmdm_2324.R;
+import com.example.pmdm_2324.ut06.datos.ut06Partido;
+
+public class ut06a1_ListaPartidos extends AppCompatActivity {
+
+    RecyclerView reyclerViewUser;
+    Button add;
+    ut06adapter_Partido adapter;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.ut06a1_lista_partidos);
+
+        reyclerViewUser = (RecyclerView) findViewById(R.id.ut03ListaContenedor);
+
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this.getApplicationContext());
+        reyclerViewUser.setLayoutManager(layoutManager);
+
+        adapter = new ut06adapter_Partido(ut06Partido.generatePartidos(ut06Partido.PARTIDOS_INICIALES));
+        reyclerViewUser.setAdapter(adapter);
+
+        add = findViewById(R.id.ut03idListaAdd);
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                adapter.add(ut06Partido.generatePartidos(ut06Partido.PARTIDOS_INICIALES));
+            }
+        });
+
+        //adapter.notifyDataSetChanged();
+    }
+}
---
+++ app/src/main/java/com/example/pmdm_2324/ut06/ut06adapter_Partido.java
@@ -0,0 +1,86 @@
+package com.example.pmdm_2324.ut06;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.pmdm_2324.R;
+import com.example.pmdm_2324.ut06.datos.ut06Partido;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class ut06adapter_Partido extends RecyclerView.Adapter<ut06adapter_Partido.ViewHolder> {
+
+    private ArrayList<ut06Partido> datos;
+
+    /**
+     * Provide a reference to the type of views that you are using
+     * (custom ViewHolder).
+     */
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        private final TextView partido;
+        private final TextView resultado;
+
+        public ViewHolder(View view) {
+            super(view);
+            // Define click listener for the ViewHolder's View
+
+            partido = (TextView) view.findViewById(R.id.ut06ListaNombre);
+            resultado = (TextView) view.findViewById(R.id.ut06ListaResultado);
+        }
+
+        public TextView getTextPartido() {
+            return partido;
+        }
+
+        public TextView getTextResultado() {
+            return resultado;
+        }
+    }
+
+    /**
+     * Initialize the dataset of the Adapter.
+     *
+     * @param dataSet String[] containing the data to populate views to be used
+     * by RecyclerView.
+     */
+    public ut06adapter_Partido(ut06Partido[] dataSet) {
+        datos = new ArrayList<ut06Partido>();
+        add(dataSet);
+    }
+
+    // Create new views (invoked by the layout manager)
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup viewGroup, int viewType) {
+        // Create a new view, which defines the UI of the list item
+        View view = LayoutInflater.from(viewGroup.getContext())
+                .inflate(R.layout.ut06l1_partido, viewGroup, false);
+
+        return new ViewHolder(view);
+    }
+
+    // Replace the contents of a view (invoked by the layout manager)
+    @Override
+    public void onBindViewHolder(ViewHolder viewHolder, final int position) {
+
+        // Get element from your dataset at this position and replace the
+        // contents of the view with that element
+        viewHolder.getTextPartido().setText(datos.get(position).local + " vs " + datos.get(position).visitante);
+        viewHolder.getTextResultado().setText(datos.get(position).resultado);
+    }
+
+    // Return the size of your dataset (invoked by the layout manager)
+    @Override
+    public int getItemCount() {
+        return datos.size();
+    }
+
+    public void add(ut06Partido[] dataSet){
+        datos.addAll(Arrays.asList(dataSet));
+        notifyDataSetChanged();
+    }
+}
